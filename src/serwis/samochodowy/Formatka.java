6c9dcd7924966912a08526a79bc62752a8a7f918
Mariusz12345 <przybylowski303@gmail.com>
2015-08-07 07:01:32 +0200

    SerwisSamochodowy

---
+++ src/serwis/samochodowy/Formatka.java
@@ -0,0 +1,40 @@
+/*
+ * Created by JFormDesigner on Fri Aug 07 07:00:04 CEST 2015
+ */
+
+package serwis.samochodowy;
+
+import javax.swing.*;
+import com.jgoodies.forms.layout.*;
+
+/**
+ * @author Adam Piotr
+ */
+public class Formatka extends JPanel {
+	public Formatka() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+		// Generated using JFormDesigner Evaluation license - Adam Piotr
+
+		//======== this ========
+
+		// JFormDesigner evaluation mark
+		setBorder(new javax.swing.border.CompoundBorder(
+			new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+				"JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+				javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+				java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+		setLayout(new FormLayout(
+			"default, $lcgap, default",
+			"2*(default, $lgap), default"));
+		// JFormDesigner - End of component initialization  //GEN-END:initComponents
+	}
+
+	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+	// Generated using JFormDesigner Evaluation license - Adam Piotr
+	// JFormDesigner - End of variables declaration  //GEN-END:variables
+}
